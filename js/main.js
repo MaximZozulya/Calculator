e21ce84f4b5f3d8e1b5e3057d4201301e7c1ae2b
Maksim Zozulya <soundmax15041987@gmail.com>
2024-11-06 14:23:36 +0300

    Implemented calculations and output of results in the console

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 'use strict';
 
+let calculation = localStorage.getItem('calculation') || '';
+
+function updateCalculation(value) {
+  calculation += value;
+  console.log(calculation);
+  localStorage.setItem('calculation', calculation);
+}
+
+function saveCalculation() {
+  localStorage.setItem('calculation', calculation);
+}
